b11e26532df77b6101f72b0f5a36165df36d3a8b
Maksim Sharoika <maximsharoika@gmail.com>
2025-02-16 22:29:22 -0600

    removing deps

--- backend-app/routes/ride.js
+++ backend-app/routes/ride.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Admin = require('../models/Admin');
-const User = require('../models/User');
 const Rider = require('../models/Rider');
 const Ride = require('../models/Ride');
 const Driver = require('../models/Driver');
-const Token = require('../models/Token');
 const router = express.Router();
 const { authenticate } = require("../middlewares/auth");
 
