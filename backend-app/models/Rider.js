2afbf7c1cb4baef5299d052ccfab14f69215e84d
Maksim Sharoika <maximsharoika@gmail.com>
2025-02-16 22:28:30 -0600

    Forgot to capitalize

--- backend-app/models/Rider.js
+++ backend-app/models/Rider.js
@@ -11,13 +11,13 @@ const RiderSchema = new mongoose.Schema({
   completedRides: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ride' }]
 });
 
-riderSchema.pre('save', async function (next) {
+RiderSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-riderSchema.methods.comparePassword = async function (password) {
+RiderSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
