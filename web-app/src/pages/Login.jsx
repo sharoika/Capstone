a33f506555f17503d3780a3d26a5c89a35fe4470
Muhammad Zaman <98908904+MTZamanpk@users.noreply.github.com>
2025-02-17 23:05:56 -0600

    api call fix

--- web-app/src/pages/Login.jsx
+++ web-app/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
 
         try {
             const response = await axios.post(
-                `${process.env.API_URL}/api/admin/login`,
+                `${process.env.REACT_APP_API_URL}/api/admin/login`,
                 loginData
             );
 
